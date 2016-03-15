59ff98be22b2278fa3ba642dac3b3edacf5942d0
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-15 23:00:07 +0530

    fect : Added logic
    Added logic for finding the multiples of 3 and 5

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    double sumThree = 0, sumFive = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0)
+            sumThree += i;
+        else if (i % 5 == 0)
+            sumFive += i;
+
+
+    }
+    cout << sumFive + sumThree << endl;
+
     return 0;
 }
