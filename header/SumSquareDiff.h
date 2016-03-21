c4d1927bc277b642591d34a87db19047bc006660
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-21 22:06:29 +0530

    fect : Class created for problem 6 Sum square Difference

---
+++ header/SumSquareDiff.h
@@ -0,0 +1,14 @@
+//
+// Created by Aditya on 21-Mar-16.
+//
+
+#ifndef PROJECTEULER_SUMSQUAREDIFF_H
+#define PROJECTEULER_SUMSQUAREDIFF_H
+
+
+class SumSquareDiff {
+
+};
+
+
+#endif //PROJECTEULER_SUMSQUAREDIFF_H
---
+++ source/SumSquareDiff.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aditya on 21-Mar-16.
+//
+
+#include "../header/SumSquareDiff.h"
