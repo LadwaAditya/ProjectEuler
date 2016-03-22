1dcc0f7c3a6ccd69044aea6c3a60a8e8f1a329d2
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-22 11:55:30 +0530

    fect : Finished Problem 6 Passing tests

--- source/PrimeThousand.cpp
+++ source/PrimeThousand.cpp
@@ -10,18 +10,18 @@ PrimeThousand::PrimeThousand() {
     bool flag = false;
     int count = 0;
     while (true) {
-        for (int i = 2; i < n / 2; i++) {
+        for (int i = 2; i <= n / 2; i++) {
             flag = n % i == 0;
             if (flag)
                 break;
         }
-        n++;
-        if (flag)
+        if (!flag)
             count++;
         if (count == 10001) {
             thousandPrime = n;
             break;
         }
+        n++;
     }
 
 }
