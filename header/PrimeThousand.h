9d9dc57bafeb772ccdee3de582a090e863da63d8
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-22 11:24:28 +0530

    fect : Created class for problem 6 10001st prime number

---
+++ header/PrimeThousand.h
@@ -0,0 +1,14 @@
+//
+// Created by Aditya on 22-Mar-16.
+//
+
+#ifndef PROJECTEULER_PRIMETHOUSAND_H
+#define PROJECTEULER_PRIMETHOUSAND_H
+
+
+class PrimeThousand {
+
+};
+
+
+#endif //PROJECTEULER_PRIMETHOUSAND_H
---
+++ source/PrimeThousand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aditya on 22-Mar-16.
+//
+
+#include "../header/PrimeThousand.h"
